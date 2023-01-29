c8343c6e8477aa96acabe38d0df6de576f2f2271
Gokhan Sari <gokhan@gokhansari.me>
2023-01-29 12:30:13 +0300

    Fix postgres id sequences after bootstrapping

--- backend/pkg/service/bootstrap/run.go
+++ backend/pkg/service/bootstrap/run.go
@@ -139,6 +139,41 @@ func Run(dp *depot.Depot, payload *Payload) (*userself.UserSelf, error) {
 			}
 		}
 
+		var sequenceFixingQueries []string
+
+		err2 = dp.Postgres().Raw(`
+			SELECT 'SELECT SETVAL('||
+				   "quote_literal"("quote_ident"("PGT"."schemaname") || '.' || "quote_ident"("S"."relname")) ||
+				   ', COALESCE(MAX(' || "quote_ident"("C"."attname")|| '), 1) ) FROM ' ||
+				   "quote_ident"("PGT"."schemaname")|| '.'||"quote_ident"("T"."relname")|| ';'
+			FROM "pg_class" AS "S",
+				 "pg_depend" AS "D",
+				 "pg_class" AS "T",
+				 "pg_attribute" AS "C",
+				 "pg_tables" AS "PGT"
+			WHERE "S"."relkind" = 'S'
+				AND "S"."oid" = "D"."objid"
+				AND "D"."refobjid" = "T"."oid"
+				AND "D"."refobjid" = "C"."attrelid"
+				AND "D"."refobjsubid" = "C"."attnum"
+				AND "T"."relname" = "PGT"."tablename"
+			ORDER BY "S"."relname";
+		`).
+			Scan(&sequenceFixingQueries).
+			Error
+		if err != nil {
+			return err
+		}
+
+		for _, sequenceFixingQuery := range sequenceFixingQueries {
+			err2 = dp.Postgres().
+				Exec(sequenceFixingQuery).
+				Error
+			if err2 != nil {
+				return err2
+			}
+		}
+
 		return nil
 	})
 	if err != nil {
