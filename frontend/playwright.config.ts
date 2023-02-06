9758b4eec137ab861bad475d6cba29febb4450cb
Gokhan Sari <gokhan@gokhansari.me>
2023-02-07 01:11:53 +0300

    Continue testing

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,12 +1,15 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 import { devices } from "@playwright/test";
 
+require("dotenv").config({ path: "../.env" });
+
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   projects: [
     { name: "chromium", use: { ...devices["Desktop Chrome"] } },
     { name: "firefox", use: { ...devices["Desktop Firefox"] } },
     { name: "webkit", use: { ...devices["Desktop Safari"] } },
+    { name: "Google Chrome", use: { channel: "chrome" } },
   ],
   reporter: "html",
   retries: process.env.CI ? 2 : 0,
@@ -15,6 +18,7 @@ const config: PlaywrightTestConfig = {
   use: {
     actionTimeout: 0,
     baseURL: process.env.FRONTEND_BASE_URL,
+    ignoreHTTPSErrors: true,
     trace: "on-first-retry",
   },
   workers: 1,
