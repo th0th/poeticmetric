1980e1f743a4c2de5f7dc88a20af92927a42abac
Gokhan Sari <gokhan@gokhansari.me>
2023-02-09 18:11:08 +0300

    Add description

--- frontend/components/Home/index.tsx
+++ frontend/components/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CallToAction, Features, FeaturesProps, Layout, Title } from "..";
+import { CallToAction, Description, Features, FeaturesProps, Layout, Title } from "..";
 import { Jumbotron } from "./Jumbotron";
 import { Preview } from "./Preview";
 
@@ -49,6 +49,10 @@ export function Home() {
   return (
     <Layout kind="website">
       <Title>Privacy-first, regulation compliant Google Analytics alternative</Title>
+      <Description>
+        PoeticMetric is a free as in freedom, open source, privacy-first and regulation-compliant web analytics tool. You can keep track of
+        your website&apos;s traffic without invading your visitors&apos; privacy.
+      </Description>
 
       <Jumbotron />
 
