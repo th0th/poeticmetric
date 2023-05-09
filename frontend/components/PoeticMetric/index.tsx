8c8ab7e0f9e4b52b823cf2085e2aaa5e66006778
Gokhan Sari <gokhan@gokhansari.me>
2023-05-09 16:14:52 +0300

    Update the github prune action

--- frontend/components/PoeticMetric/index.tsx
+++ frontend/components/PoeticMetric/index.tsx
@@ -7,7 +7,18 @@ export function PoeticMetric() {
     return getRestApiUrl("/pm.js") || null;
   }, []);
 
-  return src === null ? null : (
-    <Script async src={src} />
+  return (
+    <>
+      {src === null ? null : (
+        <Script async src={src} />
+      )}
+
+      <Script
+        async
+        data-ackee-domain-id="83316f20-b779-4b7c-9a08-5e4dac261f1d"
+        data-ackee-server="https://radiant-cendol-bb6179.netlify.app"
+        src="https://radiant-cendol-bb6179.netlify.app/companion.js"
+      ></Script>
+    </>
   );
 }
