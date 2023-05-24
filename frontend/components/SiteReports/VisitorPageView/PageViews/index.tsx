7c5f466992169c77d5db16a42ca5b54fa936e128
Gokhan Sari <gokhan@gokhansari.me>
2023-05-24 15:55:03 +0300

    Disable the curvy lince charts

--- frontend/components/SiteReports/VisitorPageView/PageViews/index.tsx
+++ frontend/components/SiteReports/VisitorPageView/PageViews/index.tsx
@@ -8,7 +8,6 @@ import { useTooltip } from "@visx/tooltip";
 import classNames from "classnames";
 import { bisector } from "d3-array";
 import { NumberValue, ScaleLinear, ScaleTime } from "d3-scale";
-import { curveMonotoneX } from "d3-shape";
 import dayjs from "dayjs";
 import React, { useCallback, useContext, useMemo } from "react";
 import { Spinner } from "react-bootstrap";
@@ -203,7 +202,6 @@ function BasePageViews({ className, parentHeight, parentWidth, ...props }: PageV
           </Group>
 
           <LinePath
-            curve={curveMonotoneX}
             data={state.data}
             defined={(d) => d.y !== undefined}
             stroke={window.getComputedStyle(document.documentElement).getPropertyValue("--bs-primary")}
