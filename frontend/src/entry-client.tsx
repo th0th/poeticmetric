c66a7d2533da396a7e6f008635b10f23edea2d0d
Gokhan Sari <gokhan@sari.me>
2024-10-19 21:24:44 +0300

    Add base folders and files

---
+++ frontend/src/entry-client.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+import { StrictMode } from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import App from './App'
+
+hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
