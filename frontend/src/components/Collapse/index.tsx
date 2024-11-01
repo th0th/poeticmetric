45dcb60f0c3bc68f19c463858e2d0bf212e26673
iibarbari <45620986+iibarbari@users.noreply.github.com>
2024-11-01 23:11:28 +0300

    fix overflow issue

--- frontend/src/components/Collapse/index.tsx
+++ frontend/src/components/Collapse/index.tsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
-import { JSX, PropsWithoutRef, useEffect, useRef, useState } from "react";
+import { CSSProperties, JSX, PropsWithoutRef, useEffect, useRef, useState } from "react";
 import styles from "./Collapse.module.css";
+
+/* This value should match with transition duration value */
+const TRANSITION_DURATION = 350;
 
 type CollapseProps = Overwrite<PropsWithoutRef<JSX.IntrinsicElements["div"]>, {
   open: boolean;
@@ -9,6 +12,7 @@ type CollapseProps = Overwrite<PropsWithoutRef<JSX.IntrinsicElements["div"]>, {
 export default function Collapse({ children, className, open, ...props }: CollapseProps) {
   const contentRef = useRef(null);
   const [contentHeight, setContentHeight] = useState<number>(0);
+  const [overflow, setOverflow] = useState<CSSProperties["overflow"]>("hidden");
 
   useEffect(() => {
     if (contentRef.current && open) {
@@ -16,10 +20,22 @@ export default function Collapse({ children, className, open, ...props }: Collap
     } else {
       setContentHeight(0);
     }
+
+    const timeout = setTimeout(() => {
+      if (open) {
+        setOverflow("visible");
+      }
+    }, TRANSITION_DURATION);
+
+    return () => {
+      clearTimeout(timeout);
+
+      setOverflow("hidden");
+    };
   }, [open]);
 
   return (
-    <div {...props} className={clsx(styles.collapse, className)} style={{ height: contentHeight }}>
+    <div {...props} className={clsx(styles.collapse, className)} style={{ height: contentHeight, overflow }}>
       <div ref={contentRef}>
         {children}
       </div>
