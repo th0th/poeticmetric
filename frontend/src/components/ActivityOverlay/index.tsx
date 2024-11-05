14f9fd8a1322af8446245f0f273430d0db032344
iibarbari <45620986+iibarbari@users.noreply.github.com>
2024-11-05 18:38:53 +0300

    remove toast altogether

---
+++ frontend/src/components/ActivityOverlay/index.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+import { JSX, PropsWithoutRef } from "react";
+import styles from "./ActivityOverlay.module.css";
+
+type ActivityOverlayProps = Overwrite<PropsWithoutRef<JSX.IntrinsicElements["div"]>, {
+  isActive: boolean;
+}>
+
+export default function ActivityOverlay({ children, className, isActive, ...props }: ActivityOverlayProps) {
+  return (
+    <div {...props} className={clsx(styles.activityOverlay, isActive && styles.active, className)}>
+      {isActive && (
+        <>
+          <div className={styles.overlay}></div>
+
+          <div className={clsx("spinner", styles.spinner)}></div>
+        </>
+      )}
+
+      {children}
+    </div>
+  );
+}
--- frontend/src/components/Bootstrap/index.tsx
+++ frontend/src/components/Bootstrap/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { useErrorBoundary } from "react-error-boundary";
 import { useForm } from "react-hook-form";
 import { Link } from "wouter";
+import ActivityOverlay from "~/components/ActivityOverlay";
 import Layout from "~/components/Layout";
 import Title from "~/components/Title";
 import { api } from "~/lib/api";
@@ -20,29 +21,33 @@ type Form = {
 
 type State = {
   isInProgress: boolean;
+  isSubmitInProgress: boolean;
 };
 
 export default function Bootstrap() {
   const [isAlreadyBootstrapped, setIsAlreadyBootstrapped] = useState<boolean>(false);
+  const [isBootstrapped, setIsBootstrapped] = useState<boolean>(false);
   const { showBoundary } = useErrorBoundary();
   const initialized = useRef(false);
-  const [state, setState] = useState<State>({ isInProgress: true });
+  const [state, setState] = useState<State>({ isInProgress: true, isSubmitInProgress: false });
   const { formState: { errors }, handleSubmit, register, setError } = useForm<Form>({});
 
   async function submit(data: Form) {
     try {
+      setState((s) => ({ ...s, isSubmitInProgress: true }));
+
       const response = await api.post("/bootstrap", data);
       const responseJson = await response.json();
 
       if (response.ok) {
-        // showToast({ message: "PoeticMetric has been successfully bootstrapped.", variant: "success" });
-
-        // setLocation("/");
+        setIsBootstrapped(true);
       } else {
         setErrors(setError, responseJson);
       }
     } catch (error) {
       showBoundary(error);
+    } finally {
+      setState((s) => ({ ...s, isSubmitInProgress: false }));
     }
   }
 
@@ -104,11 +109,33 @@ export default function Bootstrap() {
             </div>
           </div>
         </Layout>
-      ) : (
-        <Layout>
+      ) : isBootstrapped ? (
+        <Layout verticallyCenter>
           <div className="container">
             <div className={styles.title}>
-              <small className={styles.description}>Bootstrap</small>
+              <small className={styles.summary}>Bootstrap</small>
+
+              <h2 className={styles.heading}>
+                You are all set!
+              </h2>
+
+              <p>
+                PoeticMetric has been successfully initialized.
+              </p>
+
+              <div className={styles.buttonGroup}>
+                <Link className="button button-lg button-blue" to="/sign-up">
+                  Sign up to create dashboard
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      ) : (
+        <Layout verticallyCenter>
+          <div className="container">
+            <div className={styles.title}>
+              <small className={styles.summary}>Bootstrap</small>
 
               <h1 className={styles.heading}>
                 Welcome to
@@ -120,72 +147,74 @@ export default function Bootstrap() {
             </div>
 
             <div className={clsx("card", styles.card)}>
-              <form className="card-body" onSubmit={handleSubmit(submit)}>
-                <fieldset className="fieldset">
-                  <div className="form-group">
-                    <label className="form-label" htmlFor="input-user-name">Full name</label>
+              <ActivityOverlay isActive={state.isSubmitInProgress}>
+                <form className="card-body" onSubmit={handleSubmit(submit)}>
+                  <fieldset className="fieldset" disabled={state.isSubmitInProgress}>
+                    <div className="form-group">
+                      <label className="form-label" htmlFor="input-user-name">Full name</label>
 
-                    <input className="input" id="input-user-name" required {...register("userName")} />
+                      <input className={clsx("input", errors.userName && "input-invalid")} id="input-user-name" required {...register("userName")} />
 
-                    {!!errors.userName ? (<div>{errors.userName.message}</div>) : null}
-                  </div>
-
-                  <div className="form-group">
-                    <label className="form-label" htmlFor="input-user-email">E-mail address</label>
-
-                    <input
-                      className={clsx("input", errors.userEmail && "input-invalid")}
-                      id="input-user-email"
-                      required
-                      type="email"
-                      {...register("userEmail")}
-                    />
-
-                    {!!errors.userEmail ? (<div className="form-error">{errors.userEmail.message}</div>) : null}
-                  </div>
-
-                  <div className="form-group">
-                    <label className="form-label">New password</label>
-
-                    <input className="input" required type="password" {...register("userPassword")} />
-
-                    {!!errors.userPassword ? (<div className="form-error">{errors.userPassword.message}</div>) : null}
-                  </div>
-
-                  <div className="form-group">
-                    <label className="form-label">New password (again)</label>
-
-                    <input
-                      className={clsx("input", errors.userPassword2 && "input-invalid")}
-                      required
-                      type="password"
-                      {...register("userPassword2")}
-                    />
-
-                    {!!errors.userPassword2 ? (<div className="form-error">{errors.userPassword2.message}</div>) : null}
-                  </div>
-
-                  <div className="form-group">
-                    <label className="form-label">Organization</label>
-
-                    <input className="input" required {...register("organizationName")} />
-
-                    {!!errors.organizationName ? (<div className="form-error">{errors.organizationName.message}</div>) : null}
-                  </div>
-
-                  <div className="form-group">
-                    <div className="form-group-inline">
-                      <input id="input-create-demo-site" type="checkbox" {...register("createDemoSite")} />
-
-                      <label htmlFor="input-create-demo-site">Create demo site</label>
+                      {!!errors.userName ? (<div>{errors.userName.message}</div>) : null}
                     </div>
 
-                    {!!errors.createDemoSite ? (<div className="form-error">{errors.createDemoSite.message}</div>) : null}
-                  </div>
+                    <div className="form-group">
+                      <label className="form-label" htmlFor="input-user-email">E-mail address</label>
 
-                  <button className="button button-blue" type="submit">Complete installation</button>
-                </fieldset>
-              </form>
+                      <input
+                        className={clsx("input", errors.userEmail && "input-invalid")}
+                        id="input-user-email"
+                        required
+                        type="email"
+                        {...register("userEmail")}
+                      />
+
+                      {!!errors.userEmail ? (<div className="form-error">{errors.userEmail.message}</div>) : null}
+                    </div>
+
+                    <div className="form-group">
+                      <label className="form-label">New password</label>
+
+                      <input className={clsx("input", errors.userPassword && "input-invalid")} required type="password" {...register("userPassword")} />
+
+                      {!!errors.userPassword ? (<div className="form-error">{errors.userPassword.message}</div>) : null}
+                    </div>
+
+                    <div className="form-group">
+                      <label className="form-label">New password (again)</label>
+
+                      <input
+                        className={clsx("input", errors.userPassword2 && "input-invalid")}
+                        required
+                        type="password"
+                        {...register("userPassword2")}
+                      />
+
+                      {!!errors.userPassword2 ? (<div className="form-error">{errors.userPassword2.message}</div>) : null}
+                    </div>
+
+                    <div className="form-group">
+                      <label className="form-label">Organization</label>
+
+                      <input className={clsx("input", errors.organizationName && "input-invalid")} required {...register("organizationName")} />
+
+                      {!!errors.organizationName ? (<div className="form-error">{errors.organizationName.message}</div>) : null}
+                    </div>
+
+                    <div className="form-group">
+                      <div className="form-group-inline">
+                        <input className={clsx(errors.createDemoSite && "input-invalid")} id="input-create-demo-site" type="checkbox" {...register("createDemoSite")} />
+
+                        <label htmlFor="input-create-demo-site">Create demo site</label>
+                      </div>
+
+                      {!!errors.createDemoSite ? (<div className="form-error">{errors.createDemoSite.message}</div>) : null}
+                    </div>
+
+                    <button className="button button-blue" type="submit">Complete installation</button>
+                  </fieldset>
+                </form>
+              </ActivityOverlay>
             </div>
           </div>
         </Layout>
