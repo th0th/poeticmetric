98ee63cf032cb2b964f6bed3280ca3e81dc9dd70
Gokhan Sari <gokhan@sari.me>
2024-11-18 18:30:57 +0300

    Add deployment stuff

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+case "$INSTANCE" in
+  rest-api)
+    exec /usr/local/bin/poeticmetric-rest-api
+  ;;
+  scheduler)
+    exec /usr/local/bin/poeticmetric-scheduler
+  ;;
+  worker)
+    exec /usr/local/bin/poeticmetric-worker
+  ;;
+  *)
+    echo >&2 "Invalid INSTANCE."
+    exit 1
+esac
