2a527166ba14721e5830cb012fd141455f70dff9
iibarbari <45620986+iibarbari@users.noreply.github.com>
2024-11-18 19:57:53 +0300

    remove unnecessary components

--- frontend/src/components/FormGroup/index.tsx
+++ frontend/src/components/FormGroup/index.tsx
@@ -1,7 +1,5 @@
 import clsx from "clsx";
-import { useContext } from "react";
 import { useFormContext } from "react-hook-form";
-import FormGroupProvider from "~/components/FormGroup/FormGroupProvider";
 import FormGroupContext, { FormGroupContextType } from "~/contexts/FormGroupContext";
 import styles from "./FormGroup.module.css";
 
@@ -9,37 +7,36 @@ export type FormGroupProps = FormGroupContextType;
 
 export default function FormGroup(value: FormGroupProps) {
   return (
-    <FormGroupProvider value={value}>
-      <FormGroupConsumer />
-    </FormGroupProvider>
-  );
-}
+    <FormGroupContext.Provider value={value}>
+      <FormGroupContext.Consumer>
+        {({ id, inputProps: { className, ...input } = {}, labelHelper, labelText, name }) => {
+          const { clearErrors, formState: { errors }, register } = useFormContext();
+          const isCheckboxOrRadio = ["checkbox", "radio"].includes(input?.type || "");
 
-function FormGroupConsumer() {
-  const { clearErrors, formState: { errors }, register } = useFormContext();
-  const { id, inputProps: { className, ...input } = {}, labelHelper, labelText, name } = useContext(FormGroupContext);
-  const isCheckboxOrRadio = ["checkbox", "radio"].includes(input?.type || "");
+          return (
+            <div className="form-group">
+              <div className={clsx(isCheckboxOrRadio ? "form-group-inline" : "form-group")}>
+                <div className={styles.inlineLabel}>
+                  <label className="form-label" htmlFor={id}>{labelText}</label>
 
-  return (
-    <div className="form-group">
-      <div className={clsx(isCheckboxOrRadio ? "form-group-inline" : "form-group")}>
-        <div className={styles.inlineLabel}>
-          <label className="form-label" htmlFor={id}>{labelText}</label>
+                  {labelHelper}
+                </div>
 
-          {labelHelper}
-        </div>
+                <input
+                  {...input}
+                  {...register(name, { onChange: () => clearErrors() })}
+                  className={clsx("input", (!!errors[name] || !!errors.root) && "input-invalid", className)}
+                  id={id}
+                />
+              </div>
 
-        <input
-          {...input}
-          {...register(name, { onChange: () => clearErrors() })}
-          className={clsx("input", (!!errors[name] || !!errors.root) && "input-invalid", className)}
-          id={id}
-        />
-      </div>
-
-      {!!errors[name] && typeof errors[name].message === "string" ? (
-        <div>{errors[name].message}</div>
-      ) : null}
-    </div>
+              {!!errors[name] && typeof errors[name].message === "string" ? (
+                <div>{errors[name].message}</div>
+              ) : null}
+            </div>
+          );
+        }}
+      </FormGroupContext.Consumer>
+    </FormGroupContext.Provider>
   );
 }
