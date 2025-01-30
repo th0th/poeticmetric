aad03e96e19a4aabe22901fe34651c17ecbf73a3
Gokhan Sari <git@gokhan.sari.me>
2025-01-30 21:06:30 +0300

    Add 404 page (#230)
    * Add 404 page
    
    * Update useLayoutVariant.ts

--- frontend/src/components/NotFound/index.tsx
+++ frontend/src/components/NotFound/index.tsx
@@ -1,5 +1,29 @@
+import { Link } from "wouter";
+import Title from "~/components/Title";
+import ImageNotFound from "./not-found.svg";
+
 export default function NotFound() {
   return (
-    <div>Not found</div>
+    <>
+      <Title>404 - Page not found</Title>
+
+      <div className="align-items-center container d-flex flex-grow-1 py-16">
+        <div className="align-items-center row row-cols-1 row-cols-md-2">
+          <div className="col">
+            <img alt="error" className="d-block mw-100 w-32rem" src={ImageNotFound} />
+          </div>
+
+          <div className="col fs-5 text-center text-md-start">
+            <h1>404 - Page not found</h1>
+
+            <p className="mt-8">
+              The page you requested could not be found, it might be removed or had its name changed.
+            </p>
+
+            <Link className="btn btn-primary" to="/">Go to home page</Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- frontend/src/hooks/useLayoutVariant.ts
+++ frontend/src/hooks/useLayoutVariant.ts
@@ -1,27 +1,26 @@
 import { useMemo } from "react";
 import { useLocation } from "wouter";
 
-export const siteLocations: Array<string> = [
-  "/",
-  "/blog(/.*)?",
-  "/docs(/.*)?",
-  "/manifesto",
-  "/open-source",
-  "/pricing",
-  "/privacy-policy",
-  "/terms-of-service",
+export const applicationLocations: Array<string> = [
+  "/billing",
+  "/settings",
+  "/settings/organization-details",
+  "/settings/organization-deletion",
+  "/settings/password",
+  "/settings/profile",
+  "/sites",
+  "/sites/add",
+  "/sites/edit",
+  "/sites/report",
+  "/team",
+  "/team/invite",
+  "/team/edit",
 ];
 
 export default function useLayoutVariant(): LayoutVariant {
   const [location] = useLocation();
 
   return useMemo(() => {
-    for (const d of siteLocations) {
-      if (new RegExp(`^${d}$`).test(location)) {
-        return "site";
-      }
-    }
-
-    return "application";
+    return applicationLocations.includes(location) ? "application" : "site";
   }, [location]);
 }
