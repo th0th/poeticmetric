57705c433baf8c670005d2866efc581c472010c3
Gokhan Sari <gokhan@poeticmetric.com>
2025-04-12 22:38:22 +0200

    Prevent deleting the organization owner

--- frontend/src/components/Sites/DeleteModal/index.tsx
+++ frontend/src/components/Sites/DeleteModal/index.tsx
@@ -1,35 +1,34 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { Modal } from "react-bootstrap";
 import { useErrorBoundary } from "react-error-boundary";
 import { useLocation, useSearchParams } from "wouter";
 import Portal from "~/components/Portal";
+import useSite from "~/hooks/api/useSite";
 import useSites from "~/hooks/api/useSites";
 import { api } from "~/lib/api";
 import { getUpdatedSearch } from "~/lib/router";
 
 type State = {
+  isHiding: boolean;
   isInProgress: boolean;
-  isShown: boolean;
-  site: HydratedSite | null;
 };
 
 export default function DeleteModal() {
   const { showBoundary } = useErrorBoundary();
   const [location, navigate] = useLocation();
   const [searchParams] = useSearchParams();
-  const [state, setState] = useState<State>({ isInProgress: false, isShown: false, site: null });
-  const isEnabled = useMemo(() => searchParams.get("action") === "delete", [searchParams]);
-  const siteID = useMemo(() => Number(searchParams.get("siteID")) || null, [searchParams]);
+  const [state, setState] = useState<State>({ isHiding: false, isInProgress: false });
+  const siteID = useMemo(() => Number(searchParams.get("siteID")) || undefined, [searchParams]);
+  const { data: site, error: siteError } = useSite(siteID);
   const { mutate } = useSites();
 
-  async function confirmDeletion() {
+  async function confirm() {
     try {
       setState((s) => ({ ...s, isInProgress: true }));
       const response = await api.delete(`/sites/${siteID}`);
 
       if (response.ok) {
         await mutate();
-
         hide();
       }
     } catch (e) {
@@ -41,65 +40,62 @@ export default function DeleteModal() {
 
   function handleExited() {
     navigate(`${location}${getUpdatedSearch(searchParams, { action: null, siteID: null })}`, { replace: true });
+    setState((s) => ({ ...s, isHiding: false }));
   }
 
   function hide() {
-    setState((s) => ({ ...s, isShown: false }));
+    setState((s) => ({ ...s, isHiding: true }));
   }
-
-  useEffect(() => {
-    if (isEnabled && siteID !== null) {
-      setState((s) => ({ ...s, isShown: true }));
-    }
-  }, [isEnabled, siteID]);
-
-  useEffect(() => {
-    async function getSite() {
-      try {
-        if (siteID === null) {
-          return;
-        }
-
-        const response = await api.get(`/sites/${siteID}`);
-
-        if (response.ok) {
-          const site = await response.json();
-          setState((s) => ({ ...s, site }));
-        }
-      } catch (e) {
-        showBoundary(e);
-      }
-    }
-
-    getSite().catch((e) => {
-      showBoundary(e);
-    });
-  }, [showBoundary, siteID]);
 
   return (
     <Portal>
-      <Modal centered onExited={handleExited} onHide={hide} show={state.isShown}>
+      <Modal centered onExited={handleExited} onHide={hide} show={siteID !== undefined && !state.isHiding}>
         <Modal.Header closeButton>
           <Modal.Title>Delete site</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          Are you sure you want to delete site
-          {" "}
-          <span className="fw-semi-bold">{state.site?.name}</span>
-          ?
+          {site !== undefined ? (
+            <>
+              Are you sure you want to delete team member
+              {" "}
+              <span className="fw-semi-bold">{site?.name}</span>
+              ?
+            </>
+          ) : (
+            <>
+              {siteError !== undefined ? (
+                <div>
+                  <span className="fw-bold">An error occurred:</span>
+                  {" "}
+                  {siteError.message}
+                </div>
+              ) : (
+                <div className="d-flex justify-content-center">
+                  <div className="spinner spinner-border text-primary" role="status" />
+                </div>
+              )}
+            </>
+          )}
         </Modal.Body>
 
         <Modal.Footer as="fieldset" disabled={state.isInProgress}>
           <button className="btn btn-secondary" onClick={hide} type="button">Cancel</button>
 
-          <button className="align-items-center btn btn-danger d-flex gap-4" onClick={confirmDeletion} type="button">
-            {state.isInProgress ? (
-              <span className="spinner-border spinner-border-sm" />
-            ) : null}
+          {siteError === undefined ? (
+            <button
+              className="align-items-center btn btn-danger d-flex gap-4"
+              disabled={site === undefined}
+              onClick={confirm}
+              type="button"
+            >
+              {state.isInProgress ? (
+                <span className="spinner-border spinner-border-sm" />
+              ) : null}
 
-            Delete
-          </button>
+              Delete
+            </button>
+          ) : null}
         </Modal.Footer>
       </Modal>
     </Portal>
