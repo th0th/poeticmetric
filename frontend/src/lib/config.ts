0e164b19d12591a8df432c18c11c8701866aafcd
Gokhan Sari <gokhan@sari.me>
2025-05-11 10:23:49 +0200

    Update config.ts

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,3 +1,2 @@
 export const isHosted = import.meta.env.VITE_IS_HOSTED === "true";
-// export const tagsEnvironment = import.meta.env.VITE_TAGS_ENVIRONMENT;
-export const tagsEnvironment = "development";
+export const tagsEnvironment = import.meta.env.VITE_TAGS_ENVIRONMENT;
