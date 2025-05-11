f870e87c23cf80489eae67d21fa1b3ded0652693
Gokhan Sari <gokhan@sari.me>
2025-05-11 16:59:07 +0200

    Update getHtmlFromTemplate.js

--- frontend/scripts/getHtmlFromTemplate.js
+++ frontend/scripts/getHtmlFromTemplate.js
@@ -1,3 +1,3 @@
 export default function getHtmlFromTemplate(template, rendered) {
-  return template.replace("<!--head-->", rendered.head).replace("<!--b-->", rendered.html);
+  return template.replace("<!--head-->", rendered.head).replace("<!--body-->", rendered.body);
 }
