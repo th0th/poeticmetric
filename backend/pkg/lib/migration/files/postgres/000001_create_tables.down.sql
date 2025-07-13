eb3faa01c59c32acb885d78ed4894b76875b1584
Gokhan Sari <git@gokhan.sari.me>
2025-07-13 13:00:50 +0200

    Update SQL code style

--- backend/pkg/lib/migration/files/postgres/000001_create_tables.down.sql
+++ backend/pkg/lib/migration/files/postgres/000001_create_tables.down.sql
@@ -1,9 +1,9 @@
 DROP TRIGGER organizations_after_insert ON organizations;
 DROP FUNCTION notify_after_insert();
 
-DROP TABLE "sites";
-DROP TABLE "user_access_tokens";
-DROP TABLE "users";
-DROP TABLE "organization_deletions";
-DROP TABLE "organizations";
-DROP TABLE "plans";
+DROP TABLE sites;
+DROP TABLE user_access_tokens;
+DROP TABLE users;
+DROP TABLE organization_deletions;
+DROP TABLE organizations;
+DROP TABLE plans;
